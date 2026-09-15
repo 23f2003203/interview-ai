755da5aec961c60f1ebfed71abe2b66bdb765de9
23f2003203 <23f2003203@ds.study.iitm.ac.in>
2026-09-15 15:20:23 +0530

    Change API base URL to production server

--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-ai-vqst.onrender.com",
     withCredentials: true,
 });
 
