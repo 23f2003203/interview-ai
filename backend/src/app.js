3d8346b53fd47a475203ef61d4e44ac06f3283bc
23f2003203 <23f2003203@ds.study.iitm.ac.in>
2026-09-15 15:24:24 +0530

    Update CORS origin to production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interview-ai-gd3j.vercel.app",
     credentials: true
 }))
 
